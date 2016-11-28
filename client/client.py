911a608b267d453d244a3ecbdc64150f702e03d2
David Fialho <fialho.david@gmail.com>
2016-11-28 19:38:19 +0000

    Implemented the invite() method
    Added server_url to the client

--- client/client.py
+++ client/client.py
@@ -1,11 +1,15 @@
 from utils.crypto import rsa
+from utils.requests.ack_response import AckResponse
+from utils.requests.connection import Connection
+from utils.requests.invite_request import InviteRequest
 from utils.requests.signer import Signer
 
 
 class Client(Signer):
     """ Interface for the client """
 
-    def __init__(self, email: str, key_filepath=None):
+    def __init__(self, server_url: str, email: str, key_filepath=None):
+        self.server_url = server_url
         self.email = email
 
         if key_filepath:
@@ -35,8 +39,22 @@ class Client(Signer):
         """
         return self.privkey.sign(data)
 
-    def invite(self, invitee, invitee_email):
-        pass
+    def invite(self, invitee: str, invitee_email: str):
+        """
+        Invites a new client to the group.
+
+        :param invitee: invited client's ID.
+        :param invitee_email: invited client's email.
+        """
+        with Connection(self.server_url) as connection:
+            connection.request(InviteRequest.signed_request(
+                inviter=self,
+                invitee_id=invitee.encode(),
+                invitee_email=invitee_email,
+            ))
+
+            # TODO check for errors
+            connection.get_response(AckResponse)
 
     def join(self, secret_code):
         pass
