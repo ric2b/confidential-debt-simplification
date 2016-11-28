cf22c59bb61c36f7513755f76d364013b6e69c3b
David Fialho <fialho.david@gmail.com>
2016-11-28 16:43:32 +0000

    Added JSON encoder with support for base64

---
+++ utils/requests/test_base64_json_encoder.py
@@ -0,0 +1,9 @@
+import json
+
+from utils.requests.base64_json_encoder import Base64Encoder
+
+
+class TestBase64Encoder:
+
+    def test_EncodingByteStringABC_JSONStringABC(self):
+        assert json.dumps(b"ABC", cls=Base64Encoder) == '"ABC"'
