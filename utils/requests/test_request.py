a380fbd6116ce5cbd2ab5af7b24e3dfb478fa46e
David Fialho <fialho.david@gmail.com>
2016-12-01 16:16:44 +0000

    Adjusted the names of the request tests to read_body

--- utils/requests/test_request.py
+++ utils/requests/test_request.py
@@ -13,7 +13,7 @@ class TestRequest:
     # the loading of the JSON string works
     #
 
-    def test_body_to_parameters_ValidJSONWithTwoObjects_DictWithTheTwoParameters(self):
+    def test_read_body_ValidJSONWithTwoObjects_DictWithTheTwoParameters(self):
         body = b'{"parameter1": "value1", "parameter2": "value2"}'
 
         parameters = Request._read_body(body)
@@ -23,20 +23,20 @@ class TestRequest:
             "parameter2": "value2"
         }
 
-    def test_body_to_parameters_JSONWithNoObjects_EmptyDict(self):
+    def test_read_body_JSONWithNoObjects_EmptyDict(self):
         body = b'{}'
 
         parameters = Request._read_body(body)
 
         assert parameters == {}
 
-    def test_body_to_parameters_EmptyString_RaisesRequestDecodeError(self):
+    def test_read_body_EmptyString_RaisesRequestDecodeError(self):
         body = b''
 
         with pytest.raises(RequestDecodeError):
             Request._read_body(body)
 
-    def test_body_to_parameters_ValidJSONWithArray_RaisesRequestDecodeError(self):
+    def test_read_body_ValidJSONWithArray_RaisesRequestDecodeError(self):
         body = b'["parameter1", "parameter2"]'
 
         with pytest.raises(RequestDecodeError):
