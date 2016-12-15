c620e7aa37ed0c7d6b3161a4d2327d922ce74eab
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2016-12-15 00:27:59 +0000

    Added two new requests/responses and a file to re-use RSA keys when testing.

--- utils/messages/requests.py
+++ utils/messages/requests.py
@@ -15,6 +15,40 @@ class RegisterGroup(Message):
     }
 
 
+class GroupServerJoin(Message):
+    """
+    Sent to the Group server by a user to join a group in the Group server.
+    This can only be done after the user receives his secret code by the e-mail the
+    Group server sends after another user invites him.
+    """
+
+    message_type = 'join-group'
+
+    parameter_types = {
+        'group_uuid': str,
+        'user': str,
+        'secret_code': str,
+        'inviter_signature': str
+    }
+
+
+class MainServerJoin(Message):
+    """
+    Sent to the Main Server by a user to join a group in the Main server.
+    This can only be allowed after the user does a GroupServerJoin, so that the user
+    can get his key signed by the group server.
+    """
+
+    message_type = 'join-group'
+
+    parameter_types = {
+        'group_uuid': str,
+        'user': str,
+        'user_signature': str,
+        'group_server_signature': str
+    }
+
+
 class UserInvite(Message):
     """
     Invite request sent by a valid user to invite a new user into the group.
