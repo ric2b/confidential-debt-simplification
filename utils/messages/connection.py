52fb5f363e4ffd54a73bd2a9333a581b4cc9a4b2
David Fialho <fialho.david@gmail.com>
2016-12-26 12:35:54 +0000

    Fix: moved connection to messages package

--- utils/messages/connection.py
+++ utils/messages/connection.py
@@ -3,27 +3,65 @@ import http.client as http
 from utils.messages.message import Message
 
 
+class BadRequestError(Exception):
+    """ Raised when the response's status code is '400 Bad Request' """
+    pass
+
+
+class UnauthorizedError(Exception):
+    """ Raised when the response's status code is '401 Unauthorized' """
+    pass
+
+
+class ForbiddenError(Exception):
+    """ Raised when the response's status code is '403 Forbidden' """
+    pass
+
+
+class ConflictError(Exception):
+    """ Raised when the response's status code is '409 Conflict' """
+    pass
+
+
+class NotFoundError(Exception):
+    """ Raised when the response's status code is '404 Not Found' """
+    pass
+
+
+class UnknownError(Exception):
+    """ Raised when the response's status code is not none of the expected """
+    pass
+
+
 def connect(server_url):
-    return Connection(server_url)
+    """ Entry method to connect to a server using an URL """
+    return Connection(server_url, http.HTTPConnection(server_url))
 
 
 class Connection:
     """
     Abstraction of an HTTP or HTTPS connection which supports our own request
     and response API.
+
+    IMPORTANT: To create a connection object do not use its initializer
+    directly, instead use the 'connect' factory method.
+    Might seem silly, but this separation facilitates testing.
     """
 
-    def __init__(self, server_url):
+    def __init__(self, server_url, http_connection):
         """
-        Initializes a connection with the HTTP server listening in the given
-        URL. It really creates a connection that should be closed after
-        it is no longer required.
+        THIS INITIALIZER SHOULD NOT BE USED - use the 'connect' function instead
+
+        Initializes a connection to the server with the given URL. It takes
+        an already established http connection with the corresponding server.
+        This connection will be used underneath to send and receive messages.
 
         :param server_url: URL of the HTTP server to connect to in the format
                            "address:port"
+        :param http_connection: established HTTP connection with the server
         """
         self.server_url = server_url
-        self._http_connection = http.HTTPConnection(server_url)
+        self._http_connection = http_connection
 
     def close(self):
         """
@@ -45,7 +83,7 @@ class Connection:
         self.close()
 
     #
-    # Public Interface to make messages
+    # Public Interface to make requests
     #
 
     def request(self, request: Message):
@@ -61,12 +99,22 @@ class Connection:
         """
         self._http_connection.request(
             method='POST',
-            url='/fakeserver/' + request.message_type,
-            body=request.body
+            url='/' + request.message_type,
+            body=request.dumps()
         )
 
-    def get_response(self, response_impl):
-        return Message.load_response(
-            self._http_connection.getresponse(),
-            response_impl
-        )
+    def get_response(self, response_type: Message):
+        """
+        Waits for a response to a request. The expected type of the request
+        must be provided. The main job of this method is to take an
+        HTTPResponse and load the respective response message object. In case
+        of an unsuccessful response from the server an exception is raised
+        accordingly to the HTTP error code.
+
+        :param response_type: type for the expected response
+        :return: response message from the server.
+        """
+        http_response = self._http_connection.getresponse()
+        body = http_response.read()
+
+        return response_type.load_response(body)
