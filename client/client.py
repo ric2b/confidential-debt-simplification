d054eab1c0955d3b91d4c57d0686c2d5e1b729bf
David Fialho <fialho.david@gmail.com>
2016-12-26 15:10:18 +0000

    Cleared all client code

--- client/client.py
+++ client/client.py
@@ -3,13 +3,6 @@ import logging
 from cryptography.exceptions import InvalidSignature
 
 from utils.crypto import rsa
-from utils.requests.ack_response import AckResponse
-from utils.requests.confirm_join_request import ConfirmJoinRequest
-from utils.requests.connection import Connection, connect
-from utils.requests.invite_request import InviteRequest
-from utils.requests.join_request import JoinRequest
-from utils.requests.join_response import JoinResponse
-from utils.requests.signer import Signer
 
 
 class SecurityError(Exception):
@@ -27,17 +20,17 @@ logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.DEBUG)
 
 
-class Client(Signer):
+class Client:
     """ Interface for the client """
 
-    def __init__(self, register_server_url: str,
-                 register_server_key: str,
+    def __init__(self, group_server_url: str,
+                 group_server_pubkey: str,
                  main_server_pubkey: str,
                  email: str, key_filepath=None):
 
-        self.register_server_url = register_server_url
-        self.register_server_key = register_server_key.encode()
-        self.main_server_key = main_server_pubkey.encode()
+        self.group_server_url = group_server_url
+        self.group_server_pubkey = group_server_pubkey
+        self.main_server_pubkey = main_server_pubkey
         self.email = email
 
         if key_filepath:
@@ -49,22 +42,8 @@ class Client(Signer):
         self.proxy_server_pubkey = ""
 
     @property
-    def id(self) -> bytes:
-        """
-        Returns the ID of the client. The ID of a client is a base64 string
-        representation of its public key.
-        """
-        return bytes(self.pubkey)
-
-    def sign(self, *data: bytes) -> bytes:
-        """
-        Takes a some data in bytes format and signs it using the client's
-        private key. The returned signature can only be verified. It is not
-        possible to decrypt the signature to obtain the signed data.
-
-        :returns: signature for the given data in bytes format.
-        """
-        return self.privkey.sign(*data)
+    def id(self) -> str:
+        return self.pubkey
 
     def invite(self, invitee_id: str, invitee_email: str):
         """
@@ -73,21 +52,7 @@ class Client(Signer):
         :param invitee_id: invited client's ID.
         :param invitee_email: invited client's email.
         """
-        with connect(self.register_server_url) as connection:
-            request = InviteRequest.signed(
-                inviter=self,
-                invitee_id=invitee_id.encode(),
-                invitee_email=invitee_email,
-            )
-
-            logger.debug("Sending 'invite' request for user id='%s' "
-                         "email='%s'" % (invitee_id, invitee_email))
-            connection.request(request)
-            logger.debug("Finished sending request")
-
-            connection.get_response(AckResponse)
-            logger.info("Sent 'invite' to user id='%s' email='%s'" %
-                        (invitee_id, invitee_email))
+        pass
 
     def join(self, secret_code: str, inviter_id: str):
         """
@@ -98,60 +63,7 @@ class Client(Signer):
         :raise SecurityError: if the main server signature or the inviter
                               signature do not verify.
         """
-        # inviter ID is needed in bytes format
-        # we expect a string since the inviter ID argument here comes
-        # directly from the user input and that needs to be a string
-        inviter_id = inviter_id.encode()
-
-        with connect(self.register_server_url) as connection:
-            request = JoinRequest.signed(
-                joiner=self,
-                secret_code=secret_code
-            )
-
-            logger.debug("Sending 'join' request with code='%s'" % secret_code)
-            connection.request(request)
-            logger.debug("Finished sending request")
-
-            response = connection.get_response(JoinResponse)  # type: JoinResponse
-
-            # TODO should we keep the connection for the second request or
-            # should we close and create a new one
-            # Take into account that a TLS connection might take time
-
-            try:
-                logger.debug("Verifying the response...")
-
-                response.verify(
-                    inviter=inviter_id,
-                    invitee=self.id,
-                    invitee_email=self.email,
-                    main_server_key=self.main_server_key,
-                    register_server_key=self.register_server_key
-                )
-
-                logger.debug("Response was valid")
-
-            except InvalidSignature:
-                logger.warning("Response failed verification")
-                raise SecurityError("There was an identify error with the "
-                                    "response from the server")
-
-            # TODO store signed invite from inviter
-            # TODO store signed invite from request server
-
-            confirm_request = ConfirmJoinRequest.signed(
-                joiner=self,
-                inviter_id=inviter_id,
-                joiner_email=self.email
-            )
-
-            logger.debug("Sending 'confirm join'")
-            connection.request(confirm_request)
-            logger.debug("Finished sending request")
-
-            connection.get_response(AckResponse)
-            logger.info("Sent 'confirm join'")
+        pass
 
     def issue_UOMe(self, borrower, amount):
         pass
