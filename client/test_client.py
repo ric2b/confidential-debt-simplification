a5d55cde9f601636ba15e7ed8ab7e8d0bf945344
David Fialho <fialho.david@gmail.com>
2016-12-29 17:16:03 +0000

    Added fixture 'predictable signatures'

--- client/test_client.py
+++ client/test_client.py
@@ -6,12 +6,39 @@ from pytest import raises
 import client.client as c
 import utils.messages.message as m
 import utils.messages.message_formats as msg
+import utils.crypto.rsa as rsa
 from client.client import Client
 from utils.messages.connection import ConflictError, ForbiddenError, \
     UnauthorizedError
 
 
 class TestClient:
+
+    @fixture
+    def predictable_signatures(self):
+        """
+        Forces the signatures to be predictable. A signature is always in the
+        format 'key:param1-param2-...' for instance if the key is pC1 and the
+        data values to be signed are A and B then the signature is 'pC1-A-B'.
+
+        In order to be able to verify a signature signed with the private key
+        using the public key, it must be possible to obtain the private key
+        from the public key. To accomplish this, the private key is equal to
+        the public key but prefixed by a lower case 'p'.
+        """
+
+        def sign(key, *values):
+            return "%s:%s" % (key, "-".join(values))
+
+        def verify(pubkey, signature, *values):
+            obtained_signature = "p%s:%s" % (pubkey, "-".join(values))
+
+            if obtained_signature != signature:
+                raise rsa.InvalidSignature()
+
+        # create a predictable signing method
+        m.sign = Mock(side_effect=sign)
+        m.verify = Mock(side_effect=verify)
 
     @fixture
     def client(self):
@@ -33,15 +60,10 @@ class TestClient:
         # a normal connection
         c.connect = Mock(return_value=mock_connection)
 
-        def sign(key, *values):
-            return "%s:%s" % (key, "-".join(values))
-
-        # create a predictable signing method
-        m.sign = Mock(side_effect=sign)
-
         return mock_connection
 
-    def test_invite_UserC2(self, client, mock_connection):
+    def test_invite_UserC2(self, client, mock_connection,
+                           predictable_signatures):
         client.invite("C2", "c2@email.com")
 
         mock_connection.request.assert_called_once_with(
@@ -55,21 +77,21 @@ class TestClient:
         )
 
     def test_invite_UserC2WhichAlreadyExists_RaisesClientExistsError(
-            self, client, mock_connection):
+            self, client, mock_connection, predictable_signatures):
         mock_connection.get_response.side_effect = ConflictError()
 
         with raises(c.ClientExistsError):
             client.invite("C2", "c2@email.com")
 
     def test_invite_UserC2ButInviterIsNotRegistered_RaisesForbiddenError(
-            self, client, mock_connection):
+            self, client, mock_connection, predictable_signatures):
         mock_connection.get_response.side_effect = ForbiddenError()
 
         with raises(c.PermissionDeniedError):
             client.invite("C2", "c2@email.com")
 
     def test_invite_UserC2ButSignatureWasNotAccepted_RaisesSecurityError(
-            self, client, mock_connection):
+            self, client, mock_connection, predictable_signatures):
         mock_connection.get_response.side_effect = UnauthorizedError()
 
         with raises(c.SecurityError):
