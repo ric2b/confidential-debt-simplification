832f5face48f8761a23bf250d525212a6daf5669
David Fialho <fialho.david@gmail.com>
2017-01-01 12:34:09 +0000

    Added loading of the configurations

--- client/main.py
+++ client/main.py
@@ -1,17 +1,21 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 
+from configuration import config
 from main_window import MainWindow
 
 
 def main():
     app = QApplication(sys.argv)
-    window = MainWindow()
 
+    config_path = "client.json"
+    config.load(config_path)
+
+    window = MainWindow()
     window.adjustSize()
     window.show()
     sys.exit(app.exec_())
 
 
-if __name__ == '__main__':              # if we're running file directly and not importing it
-    main()                              # run the main function
+if __name__ == '__main__':
+    main()
