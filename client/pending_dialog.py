0d9c613809392458d6c90c47857ab29132b9e09a
David Fialho <fialho.david@gmail.com>
2017-01-03 12:17:37 +0000

    Reimplemented the pensing dialog to support the new redesigned interface

--- client/pending_dialog.py
+++ client/pending_dialog.py
@@ -1,7 +1,5 @@
 from PyQt5.QtCore import Qt
-
 from PyQt5.QtWidgets import QDialog
-from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtWidgets import QTableWidgetItem
 
 from ui_pending import Ui_PendingDialog
@@ -18,46 +16,85 @@ class PendingDialog(QDialog):
         self.ui.setupUi(self)
 
         # Connect buttons to respective slots
-        self.ui.button_box.accepted.connect(self.accept_uome)
         apply_button = self.ui.button_box.button(self.ui.button_box.Apply)
-        apply_button.clicked.connect(self.accept_uome)
-        self.ui.select_all_button.clicked.connect(self.select_all)
-        self.ui.select_none_button.clicked.connect(self.select_none)
+        apply_button.clicked.connect(self.apply)
+
+    def accept(self):
+        self.apply()
+
+        # Call the accept method of the QDialog
+        super(PendingDialog, self).accept()
+
+    def apply(self):
+        if self.ui.tab_widget.currentIndex() == 0:
+            # loans tab is active
+            self.cancel_uome()
+        else:
+            # debts tab is active
+            self.accept_uome()
 
     def accept_uome(self):
         remove_rows = []
 
-        for row in range(self.ui.table.rowCount()):
-            item = self.ui.table.item(row, 0)
+        for row in range(self.ui.loans_table.rowCount()):
+            item = self.ui.loans_table.item(row, 0)
             if item.checkState() == Qt.Checked:
                 self.client.accept_UOMe(item.text())
                 remove_rows.append(row)
 
         for i, row in enumerate(remove_rows):
-            self.ui.table.removeRow(row - i)
+            self.ui.loans_table.removeRow(row - i)
+
+    def cancel_uome(self):
+        remove_rows = []
+
+        for row in range(self.ui.debts_table.rowCount()):
+            item = self.ui.debts_table.item(row, 0)
+            if item.checkState() == Qt.Checked:
+                self.client.cancel_UOMe(item.text())
+                remove_rows.append(row)
+
+        for i, row in enumerate(remove_rows):
+            self.ui.debts_table.removeRow(row - i)
 
     def refresh(self):
-        ignored, uomes = self.client.pending_UOMes()
+        loans, debts = self.client.pending_UOMes()
 
-        # Fill the table with each UOMe
-        self.ui.table.setRowCount(len(uomes))
+        # Update both tables
+        self._refresh_table(self.ui.loans_table, loans)
+        self._refresh_table(self.ui.debts_table, debts)
+
+    @staticmethod
+    def _refresh_table(table, uomes):
+        # Fill the loans table with each UOMe
+        table.setRowCount(len(uomes))
         for i, uome in enumerate(uomes):
             # Add a check box for each UOMe
             item = QTableWidgetItem(uome.uuid)
             item.setCheckState(Qt.Unchecked)
-            self.ui.table.setItem(i, 0, item)
+            table.setItem(i, 0, item)
 
-            self.ui.table.setItem(i, 1, QTableWidgetItem(uome.borrower))
-            self.ui.table.setItem(i, 2, QTableWidgetItem(str(uome.value)))
-            self.ui.table.setItem(i, 3, QTableWidgetItem(uome.description))
+            table.setItem(i, 1, QTableWidgetItem(uome.borrower))
+            table.setItem(i, 2, QTableWidgetItem(str(uome.value)))
+            table.setItem(i, 3, QTableWidgetItem(uome.description))
 
     def select_all(self):
-        for row in range(self.ui.table.rowCount()):
-            self.ui.table.item(row, 0).setCheckState(Qt.Checked)
+        table = self._current_table()
+        for row in range(table.rowCount()):
+            table.item(row, 0).setCheckState(Qt.Checked)
 
     def select_none(self):
-        for row in range(self.ui.table.rowCount()):
-            self.ui.table.item(row, 0).setCheckState(Qt.Unchecked)
+        table = self._current_table()
+        for row in range(table.rowCount()):
+            table.item(row, 0).setCheckState(Qt.Unchecked)
+
+    def _current_table(self):
+        if self.ui.tab_widget.currentIndex() == 0:
+            # loans tab is active
+            return self.ui.loans_table
+        else:
+            # debts tab is active
+            return self.ui.debts_table
 
 
 
