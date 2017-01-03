e8d9446ac8ce631aa9622b47b70fb88ad5343a89
David Fialho <fialho.david@gmail.com>
2017-01-03 13:05:02 +0000

    Added accept button

---
+++ client/accept_button.py
@@ -0,0 +1,15 @@
+from commit_button import CommitButton
+
+
+class AcceptButton(CommitButton):
+
+    COLUMN = 0
+
+    def __init__(self, client, table, row):
+        super().__init__(table, row)
+        self.client = client
+        self.setText("accept")
+
+    def operate(self, uome_uuid):
+        """ Subclasses should override this method to accept or cancel """
+        self.client.accept_UOMe(uome_uuid)
