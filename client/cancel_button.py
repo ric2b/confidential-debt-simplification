5e0de2839cc0efca3c076b7218df662b9e057d37
David Fialho <fialho.david@gmail.com>
2017-01-03 13:10:16 +0000

    Added cancel button

---
+++ client/cancel_button.py
@@ -0,0 +1,15 @@
+from commit_button import CommitButton
+
+
+class CancelButton(CommitButton):
+
+    COLUMN = 0
+
+    def __init__(self, client, table, row):
+        super().__init__(table, row)
+        self.client = client
+        self.setText("cancel")
+
+    def operate(self, uome_uuid):
+        """ Subclasses should override this method to accept or cancel """
+        self.client.cancel_UOMe(uome_uuid)
